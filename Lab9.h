5bd779f2daf55c7d20012ec80f3c2dd6b68850a8
Jokex3 <kennymjose@gmail.com>
2019-07-28 11:28:35 -0400

    Modificar para evitar errores y agregar maximo

--- Lab9.h
+++ Lab9.h
@@ -26,35 +26,35 @@ unsigned int operacionHash(char *palabra,int tamTabla){
 
 Nodo *crearNodo(char *palabra){
     int largo = strlen(palabra); //Strlen permite obtener el largo de un string sin contar \0, ej: hola, su largo seria 4
-    Nodo *nodoAux = malloc(sizeof(struct Tnodo));
+    Nodo *nodoAux = (Nodo *)malloc(sizeof(struct Tnodo));
     nodoAux->sgte = NULL;
-    nodoAux->palabra = malloc(sizeof(char)*(largo+1)); //Pedir memoria de tipo char para poder hacer un arreglo con el largo+1 para \0
+    nodoAux->palabra = (char *)malloc(sizeof(char)*(largo+1)); //Pedir memoria de tipo char para poder hacer un arreglo con el largo+1 para \0
     strcpy(nodoAux->palabra,palabra); //Copia el string de palabra y la guarda en el string de la estructura
 
     return nodoAux; //retornamos el nodo
 }
 
 //Funcion para agregar el nodo a la tabla y a la lista
-void insertarPalabra(Nodo *TablaHash[],char *palabra,int tamTabla){ 
+void insertarPalabra(Nodo *tablaHash[],char *palabra,int tamTabla){ 
     //Recordar que palabra es la palabra extraida del archivo
     int resultadoHash;
     Nodo *nodoAux1; Nodo *nodoAux2;
     resultadoHash = operacionHash(palabra,tamTabla); //resultadoHash resibe la clave (posicion) donde se guardara
-    nodoAux1 = TablaHash[resultadoHash]; //nodoAux1 guarda el puntero a la tabla
+    nodoAux1 = tablaHash[resultadoHash]; //nodoAux1 guarda el puntero a la tabla
     nodoAux2 = crearNodo(palabra); //se crea el nodoAux2 con la nueva palabra
-    TablaHash[resultadoHash] = nodoAux2; //la tabla apunta al nodo nuevo
-    TablaHash[resultadoHash]->sgte = nodoAux1; //Tabla hash su sgte contiene el puntero de la tabla que la tenia nodoAux
+    tablaHash[resultadoHash] = nodoAux2; //la tabla apunta al nodo nuevo
+    tablaHash[resultadoHash]->sgte = nodoAux1; //Tabla hash su sgte contiene el puntero de la tabla que la tenia nodoAux
 }
 
 
 //Funcion que retorna la cantidad total de palabras procesadas
-int *leerArchivo(Nodo* tablaHash[],char *nombre, int tamTabla){
+int leerArchivo(Nodo* tablaHash[],char *nombre, int tamTabla){
     strcat(nombre,".txt"); //Agrega .txt despues del ultimo caracter del string
     char *palabraExtraida;
     int totalDePalabras = 0;
     FILE *archivo = fopen(nombre,"r");
     while(!feof(archivo)){ //Mientras no sea final de archivo
-        palabraExtraida = malloc(sizeof(char)*30);
+        palabraExtraida = (char *)malloc(sizeof(char)*30);
         fscanf(archivo,"%s",palabraExtraida);
         insertarPalabra(tablaHash,palabraExtraida,tamTabla);
         totalDePalabras++;
@@ -64,4 +64,28 @@ int *leerArchivo(Nodo* tablaHash[],char *nombre, int tamTabla){
     tablaHash[0]->totalDePalabras = totalDePalabras;
 
     return totalDePalabras;
-}
+
+    int maximo(Nodo* tablaHash[], int tamTabla){
+        int maximoDePalabras; 
+        int cantidadDePalabras; //Variables para el maximo y la cantidad total de palabras que hay en la posicion
+        Nodo *nodoAux;
+        for(int cont=0; cont<tamTabla;i++){
+            nodoAux = tablaHash[cont]; //Un nodo apunta a la tabla, es decir al primero nodo de la posicion que dice cont
+            while(nodoAux){ //Mientras nodo no es nulo
+                cantidadDePalabras++; //Aumenta
+                nodoAux = nodoAux->sgte; //Se mueve de un nodo a otro en la lista
+            }
+            if(maximoDePalabras < cantidadDePalabras){ //Si maximo es menor que la cantidad de palabras
+                maximoDePalabras = cantidadDePalabras; //Maximo obtiene el valor de totalDePalabras
+            }
+            cantidadDePalabras = 0; //Vuelve a ser 0 para poder usarse nuevamente
+        }
+
+        return maximoDePalabras;
+    }
+
+    int minimo(Nodo *tablaHash,int tamTabla){
+        int minimo
+
+
+    }
